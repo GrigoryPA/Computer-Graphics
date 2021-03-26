d17b01a22e1a4bd46cd19ab1eb7fe10eba7e2099
Grigory <grial00.06@gmail.com>
2021-03-26 23:33:08 +0300

    Зеркальный пол, не зна нахуя он нужен.

--- СomputerGraphics/src/main/java/SceneIntersect.java
+++ СomputerGraphics/src/main/java/SceneIntersect.java
@@ -3,7 +3,7 @@ import java.util.Vector;
 public class SceneIntersect {
     public Vector3d hit;
     public Vector3d N;
-    public Material3D material;
+    public Material3D material = new Material3D(1);
     public boolean isIntersect=false;
 
 
@@ -19,6 +19,22 @@ public class SceneIntersect {
                 material = AllSpheres.elementAt(i).material;
             }
         }
-        isIntersect = spheres_dist < 1000;
+
+        double checkerboard_dist = Double.MAX_VALUE;
+        if(Math.abs(dir.y)>0.001){
+            double d = -(orig.y+4)/dir.y;//плоскость шахматной доски задана как y=-4
+            Vector3d pt = orig.getAddition((dir.getVectorScaled(d)));
+            if(d>0 && Math.abs(pt.x)<100 &&
+                    pt.z>0 && pt.z<1000 &&
+                    d<spheres_dist){
+                checkerboard_dist = d;
+                hit = pt;
+                N = new Vector3d(0,1,0);
+                material = new Material3D(3);
+            }
+
+        }
+
+        isIntersect = Math.min(spheres_dist, checkerboard_dist) < 1000;
     }
 }
